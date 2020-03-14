41afbc149001142f3f97929c64d01c80014891d2
Thamires Schifini <thamiresschifinif@gmail.com>
2020-03-14 03:12:28 -0300

    Fazer commit inicial

---
+++ school/urls.py
@@ -0,0 +1,30 @@
+"""school URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from django.http import HttpResponse
+
+# a view é uma função
+#function based views
+def grades(request):                #função para receber um request e retornar uma response
+    if request.method == 'GET':
+        return HttpResponse("VAI CONRINTIANS")
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('grades/', grades)
+]
